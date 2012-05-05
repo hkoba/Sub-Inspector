cf48798da5ef83696e5fbb2ab2ce06e08060962d
Hiroki Honda <cside.story@gmail.com>
2012-05-05 12:25:13 +0900

    minimum ver

--- lib/Sub/Inspector.pm
+++ lib/Sub/Inspector.pm
@@ -2,16 +2,39 @@ package Sub::Inspector;
 use 5.008_001;
 use strict;
 use warnings;
+use B ();
+use Carp ();
+use attributes;
+use Data::Dumper;
 
 our $VERSION = '0.01';
 
+sub new {
+    my ($class, $code) = @_;
+    ref $code eq 'CODE' || do {
+        local $Data::Dumper::Indent = 1;
+        local $Data::Dumper::Terse  = 1;
+        Carp::croak "argument isn't a subroutine reference: " . Dumper($code);
+    };
+    bless +{ code => $_[1] }, $_[0]
+}
+
+sub file { B::svref_2object($_[0]->{code})->GV->FILE }
+sub line { B::svref_2object($_[0]->{code})->GV->LINE }
+sub name { B::svref_2object($_[0]->{code})->GV->NAME }
+
+sub proto     { B::svref_2object($_[0]->{code})->PV }
+sub prototype { proto(@_) }
+
+sub attrs      { attributes::get($_[0]->{code}) }
+sub attributes { attrs(@_) }
 
 1;
 __END__
 
 =head1 NAME
 
-Sub::Inspector - Perl extention to do something
+Sub::Inspector - get infomation from a subroutine reference
 
 =head1 VERSION
 
@@ -21,39 +44,75 @@ This document describes Sub::Inspector version 0.01.
 
     use Sub::Inspector;
 
+    # get file, line, name
+
+    use File::Spec;
+    my $code = File::Spec->can('canonpath');
+    my $inspector = Sub::Inspector->new($code);
+
+    print $inspector->file; #=> '/Users/Cside/perl5/ ... /File/Spec/Unix.pm'
+    print $inspector->line; #=> 71
+    print $inspector->name; #=> 'canonpath'
+
+    # get prototype
+
+    use Try::Tiny;
+    my $inspector2 = Sub::Inspector->new(\&try);
+
+    print $inspector2->proto; #=> '&;@'
+
+    # get attributes
+
+    use AnyEvent::Handle;
+    my $code = AnyEvent::Handle->can('rbuf');
+    my $inspector3 = Sub::Inspector->new(\&try);
+
+    print $inspector3->attrs; #=> ('lvalue')
+
 =head1 DESCRIPTION
 
-# TODO
+This module enable to get metadata (prototype, attributes, method name, etc) from a coderef.
+We can get them by the buit-in module, B.pm. However, it is a bit difficult to memorize how to use it.
 
 =head1 INTERFACE
 
-=head2 Functions
+=head2 Methods
 
-=head3 C<< hello() >>
+=over
 
-# TODO
+=item $inspector->file
 
-=head1 DEPENDENCIES
+=item $inspector->line
 
-Perl 5.8.1 or later.
+=item $inspector->name
 
-=head1 BUGS
+=item $inspector->proto
 
-All complex software has bugs lurking in it, and this module is no
-exception. If you find a bug please either email me, or add the bug
-to cpan-RT.
+alias: prototype
+
+=item $inspector->attrs
+
+alias: attributes
+
+=back
 
 =head1 SEE ALSO
 
-L<perl>
+=over
+
+=item L<B>
+
+=item L<B::Deparser>
+
+=back
 
 =head1 AUTHOR
 
-<<YOUR NAME HERE>> E<lt><<YOUR EMAIL ADDRESS HERE>>E<gt>
+Hiroki Honda (Cside) E<lt>cside.story [at] gmail.comE<gt>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2012, <<YOUR NAME HERE>>. All rights reserved.
+Copyright (c) Hiroki Honda.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
--- t/001_basic.t
+++ t/001_basic.t
@@ -1,10 +1,55 @@
 #!perl -w
 use strict;
 use Test::More;
-
+use Project::Libs;
 use Sub::Inspector;
+use C;
+use Path::Class;
 
-# test Sub::Inspector here
-pass;
+subtest 'exception' => sub {
+    local $@;
+    eval { Sub::Inspector->new(C->can('null')) };
+    ok $@;
+};
+
+subtest 'file, line, name' => sub {
+    {
+        my $ins = Sub::Inspector->new(C->can('parent_method'));
+        is $ins->file, file(__FILE__)->dir->subdir('lib')->file('ParentClass.pm')->absolute;
+        is $ins->line, 5;
+        is $ins->name, 'parent_method';
+    }
+};
+
+subtest 'proto' => sub {
+    {
+        my $ins = Sub::Inspector->new(C->can('try'));
+        is $ins->proto,     '&;@';
+        is $ins->prototype, '&;@';
+    }
+    {
+        my $ins = Sub::Inspector->new(C->can('plain'));
+        is $ins->proto,     undef;
+        is $ins->prototype, undef;
+    }
+};
+
+subtest 'attr' => sub {
+    {
+        my $ins = Sub::Inspector->new(C->can('has_attr'));
+        is_deeply [$ins->attrs],      [qw(lvalue)];
+        is_deeply [$ins->attributes], [qw(lvalue)];
+    }
+    {
+        my $ins = Sub::Inspector->new(C->can('has_multi_attrs'));
+        is_deeply [$ins->attrs],      [qw(lvalue method)];
+        is_deeply [$ins->attributes], [qw(lvalue method)];
+    }
+    {
+        my $ins = Sub::Inspector->new(C->can('plain'));
+        is_deeply [$ins->attrs],      [];
+        is_deeply [$ins->attributes], [];
+    }
+};
 
 done_testing;
